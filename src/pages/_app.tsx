20b87a7eee123b9ac113069ef2139cb7fec6df03
808vita <its808vita@gmail.com>
2023-11-06 15:15:27 +0530

    bootstrap setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
+import React, { useEffect } from "react";
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    import("bootstrap/dist/js/bootstrap" as any);
+  }, []);
+
   return <Component {...pageProps} />
 }
